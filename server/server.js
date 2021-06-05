006f1bcf77cf3291d80b1691dc11199655fd9d3d
VictorEmanuell <victoremanuelexx@gmail.com>
2021-06-05 16:36:33 -0300

    version-1.0

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 3001 })
+const wss = new WebSocket.Server({ port: process.env.PORT })
+
+console.log('Ok')
 
 wss.on('connection', (ws) => {
     ws.on('message', message => {
         console.log(message)
+        wss.clients.forEach(client => client.send(message))
     })
-
-    ws.send('hello')
 })
